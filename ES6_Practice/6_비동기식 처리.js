dbaf7511806737314fe117adfc62709258ea662f
DESKTOP-QJ7KUJE\SW교육지원센터 <blueandgreen0530@gmail.com>
2020-09-22 19:43:09 +0900

    master

---
+++ ES6_Practice/6_비동기식 처리.js
@@ -0,0 +1,25 @@
+// 1. 동기식 add() 함수
+function addSync(a, b){
+    return a+b;
+  }
+  
+  console.log('before')
+  var result = addSync(1,2)
+  console.log('after')
+  console.log(result)
+  
+  
+  // 2. 비동기식 add() 함수
+  function addAsync(a, b, callback){
+    setTimeout(() => {
+      callback(a+b);
+    }, 5000);
+  }
+  
+  console.log('before');
+  addAsync(1, 2, function(result){
+    console.log(result);
+  });
+  console.log('after');
+
+  // setTimeout(): 
