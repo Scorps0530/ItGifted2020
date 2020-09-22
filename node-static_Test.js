a618109aca38a892bf2b7c0c3082970b92dec461
DESKTOP-QJ7KUJE\SW교육지원센터 <blueandgreen0530@gmail.com>
2020-09-22 18:26:00 +0900

    master

--- node-static_Test.js
+++ node-static_Test.js
@@ -1,1 +1,12 @@
-var http = require("node-static");
+var static = require('node-static');
+ 
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
